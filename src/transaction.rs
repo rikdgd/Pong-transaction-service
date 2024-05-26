ada4135657bf2059a63600cfdf91c39d42a781a9
Rik de Goede <rik.dgd@gmail.com>
2024-05-27 00:34:01 +0200

    fixed transaction id handling by renaming id to '_id' and skipping it when None on serialization.

--- src/transaction.rs
+++ src/transaction.rs
@@ -2,8 +2,11 @@ use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
 
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Transaction {
+    #[serde(rename = "_id")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<ObjectId>,
     pub sender_id: ObjectId,
     pub receiver_id: ObjectId,
