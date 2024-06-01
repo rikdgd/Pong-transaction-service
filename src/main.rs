5e884f231c700448a63bacd4ac7cbec17886202b
Rik de Goede <rik.dgd@gmail.com>
2024-06-01 19:59:39 +0200

    Add a simple testing GET endpoint.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ use crate::transaction::Transaction;
 use crate::transaction_service::TransactionService;
 
 
+#[get("/test")]
+async fn test_endpoint() -> &'static str {
+    "Hello world!"
+}
+
 
 #[get("/getTransaction/<transaction_id>")]
 async fn get_transaction(transaction_id: String) -> String {
@@ -19,7 +24,6 @@ async fn get_transaction(transaction_id: String) -> String {
     
     format!("{:?}", res)
 }
-
 
 
 #[get("/postTestTransaction")]
@@ -38,6 +42,7 @@ async fn post_test_transaction() -> String {
     format!("new transaction id: {}", new_id)
 }
 
+
 #[post("/createTransaction", data = "<transaction>")]
 async fn post_transaction(transaction: Json<Transaction>) -> String {
     let service = TransactionService::new().expect("Failed to get mongodb uri.");
@@ -49,6 +54,7 @@ async fn post_transaction(transaction: Json<Transaction>) -> String {
     format!("New transaction id: {}", new_id)
 }
 
+
 #[get("/getUserBalance/<user_id>")]
 async fn get_user_balance(user_id: String) -> String {
     let service = TransactionService::new().expect("Failed to get mongodb uri.");
@@ -58,9 +64,11 @@ async fn get_user_balance(user_id: String) -> String {
 }
 
 
+
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![
+        test_endpoint,
         get_transaction,
         post_test_transaction,
         post_transaction,
